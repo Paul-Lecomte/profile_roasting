bac740ab9524b4e86a81317d412c3a4487b7a447
Paul <lecomtepaulvd@gmail.com>
2025-06-27 11:13:19 +0200

    default header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            Header Content
-        </div>
+        <header className="w-full top-0 h-1/30 bg-gray-100 flex items-center justify-center border-t">
+            <div className="text-lg font-semibold">
+                Roast Your GitHub Profile
+            </div>
+        </header>
     );
 };
 
