3d8625bfc636807123c87ea13477964f0b0a6993
Paul <lecomtepaulvd@gmail.com>
2025-06-27 11:12:41 +0200

    default footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            Footer Content
-        </div>
+        <footer className="w-full bottom-0 h-1/30 bg-gray-100 flex items-center justify-center border-t" >
+            <div>
+                powered by{"Next.js"} and{"Typescript"}
+            </div>
+        </footer>
     );
 };
 
