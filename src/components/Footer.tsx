5e1b1839a3ba13196d56f4a637e9ea06078e49b6
Paul <lecomtepaulvd@gmail.com>
2025-06-27 11:35:10 +0200

    footer update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="w-full bottom-0 h-1/30 bg-gray-100 flex items-center justify-center border-t" >
-            <div>
-                powered by{"Next.js"} and{"Typescript"}
-            </div>
+        <footer className="w-full bg-white/90 border-t border-gray-100 shadow-sm py-2 flex items-center justify-center rounded-b-2xl">
+            <span className="text-gray-500 text-sm">
+                Powered by <span className="font-semibold text-indigo-500">Next.js</span> &amp; <span className="font-semibold text-blue-500">TypeScript</span>
+            </span>
         </footer>
     );
 };
